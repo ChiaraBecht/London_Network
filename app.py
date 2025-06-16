baf38de693c3d93d24a5733774f4126f76bbbda4
ChiaraBecht <c.becht@st.hanze.nl>
2025-06-16 22:38:06 +0200

    added Tabs

--- app.py
+++ app.py
@@ -84,12 +84,12 @@ st_folium(m, width=900, height=600)
 
 # adding schedule
 daily_schedule = {
-    "Monday June 23rd": [("10:30", "Abflug Memmingen"), ("11:30", "Ankunft London Stansted")],
+    "Monday June 23rd": [("10:30", "Abflug Memmingen"), ("11:30", "Ankunft London Stansted"), ("15:00 - 21:00", "Check-In Hotel")],
     "Tuesday June 24th": [("16:40", "Arrival at Warner Bros. Studio Tour"), ("17:00", "Start Warner Bros. Studio Tour")],
     "Wednesday June 25th": [("11:00", "Horizon 22")],
     "Thursday June 26th": [("", ""), ("20:00", "Six Musical Vaudeville Theatre")],
     "Friday June 27th": [(), ()],
-    "Saturday June 28th": [("09:00", "??Check-out"), ("16:20", "Rückflug London Stanted"), ("19:05", "Ankunft Memmingen")]
+    "Saturday June 28th": [("09:00 - 11:00", "Check-out"), ("16:20", "Rückflug London Stanted"), ("19:05", "Ankunft Memmingen")]
 }
 
 # Tabs for each day
